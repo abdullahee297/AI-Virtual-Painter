176e5ee314dba69954add4d6545cc51d55ed7965
abdullahee297 <232841637+abdullahee297@users.noreply.github.com>
2026-01-27 16:54:49 +0500

    Color Change

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ for imPath in myList:
 
 header = overlay[0]
 resize_header = None
+drawcolor = (0, 255, 0)
+
 
 cap = cv2.VideoCapture(0)
 
@@ -55,17 +57,18 @@ while True:
     if not success:
         break
 
-    
     # Set the header image at the top
     h, w, _ = img.shape
 
     # To do only one time to optimize
     if resize_header is None:
         resize_header = cv2.resize(header, (w, 125))
-    
+
     img[0:125, 0:w] = resize_header
 
-    
+    finger_count = 0
+
+
     rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     mp_img = mp.Image(mp.ImageFormat.SRGB,rgb)
     
@@ -76,11 +79,18 @@ while True:
             h, w, _ = img.shape
             lm_list = []
 
-            if lm_list != 0:
-                print(lm_list)
-            
             for lm in hand:
-                lm_list.append((int(lm.x*w), int(lm.y*h)))
+                lm_list.append((int(lm.x * w), int(lm.y * h)))
+
+            if len(lm_list) >= 21:
+                x1, y1 = lm_list[8]             # Index finger tip c0ordinates
+                x2, y2 = lm_list[12]            # Middle finger tip coordinates
+
+
+            else:
+                continue  
+            
+            # print(x1, y1)              
 
             for start, end in HAND_CONNECTIONS:
                 cv2.line(img, lm_list[start], lm_list[end], (0, 255, 0), 2)
@@ -88,13 +98,66 @@ while True:
             for x, y in lm_list:
                 cv2.circle(img, (x, y), 6, (255, 0, 0), -1)
 
-            # if result.handedness:
-            #     for i, hand_info in enumerate(result.handedness):
-            #         label = hand_info[0].category_name   # "Left" or "Right"
-            #         score = hand_info[0].score
 
-            # print(f"Hand {i}: {label}")
+            # Finger Count (Up/Down)
+            finger = []
+            if lm_list[4][0] > lm_list[3][0]:
+                finger_count += 1
+                finger.append(1)
+            else:
+                finger.append(0)
+
+            # Other fingers (y-axis check)
+            tips = [8, 12, 16, 20]
+            pips = [6, 10, 14, 18]
+
+            for tip, pip in zip(tips, pips):
+                if lm_list[tip][1] < lm_list[pip][1]:
+                    finger_count += 1
+                    finger.append(1)
+                else:
+                    finger.append(0)
+
+            #print(finger)
+
+            # Making the mode Condition
+            if finger[1] and finger[2]:
+                cv2.rectangle(img, (x1, y1 - 35), (x2, y2 + 35), (255, 0, 255), cv2.FILLED)
+                print("Selection Mode")
+                # Checking for the click
+                if y1 < 125:
+                    # For the first one 250 - 360
+                    if 250 < x1 < 400:
+                        header = overlay[1]             #green
+                        drawcolor = (0 , 255, 0)
+                        resize_header = None
+                    # For the Second one 450 - 570
+                    elif 450 < x1 < 650:
+                        header = overlay[2]             #red
+                        drawcolor = (0 , 0, 255)
+                        resize_header = None
+                    # For the Third one 690 - 890
+                    elif 690 < x1 < 900:
+                        header = overlay[3]             #blue
+                        drawcolor = (255, 0, 0)
+                        resize_header = None
+                    # For the Fourth one 
+                    elif 930 < x1 < 1050:
+                        header = overlay[4]             #yellow
+                        drawcolor = (0, 255, 255)
+                        resize_header = None
+                    # For the Fifth one 
+                    elif 1090 < x1 < 1250:
+                        header = overlay[5]             #eraser
+                        drawcolor = (255, 255, 255)
+                        resize_header = None
+                cv2.rectangle(img, (x1, y1 - 35), (x2, y2 + 35), drawcolor, cv2.FILLED)
                 
+            if finger[1] and not finger[2]:
+                cv2.rectangle(img, (x1, y1 - 35), (x2, y2 + 35), drawcolor, cv2.FILLED)
+                print("Drawing Mode")
+            
+
     
     cv2.imshow("AI Virtual Painter", img)
     if cv2.waitKey(1) & 0xFF == 27:
